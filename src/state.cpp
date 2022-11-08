d61a98a10b9da6cc800f55e88cc13a00b72d1237
Иван Назаров <ivan@MacBook-Air-Ivan.local>
2022-11-08 17:37:35 +0300

    modified some methods

--- src/state.cpp
+++ src/state.cpp
@@ -3,7 +3,12 @@
 #include <vector>
 #include <iostream>
 
-State::State() : board_size(81), p1Fences(10), p2Fences(10) {
+State::State() : board_size(81),
+                 p1Fences(10),
+                 p2Fences(10),
+                 p1(std::pair<uint8_t, uint8_t>(0, 4)),
+                 p2(std::pair<uint8_t, uint8_t>(8, 4))
+{
     graph = std::vector<std::vector<uint8_t>>(board_size);
 
     for(size_t i = 0; i < board_size; ++i) {
@@ -24,8 +29,6 @@ State::State() : board_size(81), p1Fences(10), p2Fences(10) {
     }
 }
 
-// void State::setP1Fences(uint8_t n) {}
-
 std::pair<uint8_t, uint8_t> State::getP1() const {
     return p1;
 }
@@ -34,15 +37,19 @@ std::pair<uint8_t, uint8_t> State::getP2() const {
     return p2;
 }
 
-void State::setP1(uint8_t row, uint8_t col) {
-    p1 = std::pair<uint8_t, uint8_t>(row, col);
+void State::setP1(std::pair<uint8_t, uint8_t> field) {
+    p1 = std::make_pair(field.first - 1, field.second - 1);
 }
 
-void State::setP2(uint8_t row, uint8_t col) {
-    p2 = std::pair<uint8_t, uint8_t>(row, col);
+void State::setP2(std::pair<uint8_t, uint8_t> field) {
+    p2 = std::make_pair(field.first - 1, field.second - 1);
 }
 
-void State::addFence(bool horizontal, uint8_t row, uint8_t col) {
+void State::addFence(bool horizontal, std::pair<uint8_t, uint8_t> fence) {
+
+    uint8_t row = fence.first;
+    uint8_t col = fence.second;
+
     assert(row >= 1 && row <= 9 && col >= 1 && col <= 9);
 
     row--;
@@ -53,42 +60,49 @@ void State::addFence(bool horizontal, uint8_t row, uint8_t col) {
     uint8_t bottomRight = bottomLeft + 1;
 
     if (horizontal) {
-        auto bottomLeftNeighbor = std::find(graph[topLeft].begin(), graph[topLeft].end(),bottomLeft);
-        graph[topLeft].erase(bottomLeftNeighbor);
-
-        auto bottomRightNeighbor = std::find(graph[topRight].begin(), graph[topRight].end(),bottomRight);
-        graph[topRight].erase(bottomRightNeighbor);
-
-        auto topLeftNeighbor = std::find(graph[bottomLeft].begin(), graph[bottomLeft].end(),topLeft);
-        graph[bottomLeft].erase(topLeftNeighbor);
-
-        auto topRightNeighbor = std::find(graph[bottomRight].begin(), graph[bottomRight].end(),topRight);
-        graph[bottomRight].erase(topRightNeighbor);
-
+        horizontalFences.push_back(fence);
+        deleteVerticalEdge(topLeft, bottomLeft);
+        deleteVerticalEdge(topRight, bottomRight);
         return;
     }
 
-    auto topRightNeighbor = std::find(graph[topLeft].begin(), graph[topLeft].end(), topRight);
-    graph[topLeft].erase(topRightNeighbor);
-
-    auto topLeftNeighbor = std::find(graph[topRight].begin(), graph[topRight].end(), topLeft);
-    graph[topRight].erase(topLeftNeighbor);
-
-    auto bottomRightNeighbor = std::find(graph[bottomLeft].begin(), graph[bottomLeft].end(), bottomRight);
-    graph[bottomLeft].erase(bottomRightNeighbor);
-
-    auto bottomLeftNeighbor = std::find(graph[bottomRight].begin(), graph[bottomRight].end(), bottomLeft);
-    graph[bottomRight].erase(bottomLeftNeighbor);
-
+    verticalFences.push_back(fence);
+    deleteHorizontalEdge(topLeft, topRight);
+    deleteHorizontalEdge(bottomLeft, bottomRight);
     return;
 }
 
+void State::deleteVerticalEdge(uint8_t top, uint8_t bottom) {
+    auto upstairsNeighbor = std::find(graph[bottom].begin(), graph[bottom].end(), top);
+    if (upstairsNeighbor != graph[bottom].end()) {
+        graph[bottom].erase(upstairsNeighbor);
+    }
+
+    auto downstairsNeighbor = std::find(graph[top].begin(), graph[top].end(), bottom);
+    if (downstairsNeighbor != graph[top].end()) {
+        graph[top].erase(downstairsNeighbor);
+    }
+}
+
+void State::deleteHorizontalEdge(uint8_t left, uint8_t right) {
+    auto rightsideNeighbor = std::find(graph[left].begin(), graph[left].end(), right);
+    if (rightsideNeighbor != graph[left].end()) {
+        graph[left].erase(rightsideNeighbor);
+    }
+
+    auto leftsideNeighbor = std::find(graph[right].begin(), graph[right].end(), left);
+    if (leftsideNeighbor != graph[right].end()) {
+        graph[right].erase(leftsideNeighbor);
+    }
+}
 
 void State::dirtyPrint() const {
     for (size_t i = 0; i < graph.size(); ++i) {
         std::cout << "Vertex = " << i << ": [";
-        for (size_t j = 0; j < graph[i].size(); ++j) {
-            std::cout << unsigned (graph[i][j]) << ' ';
+        if (graph[i].size() != 0) {
+            for (size_t j = 0; j < graph[i].size(); ++j) {
+                std::cout << unsigned(graph[i][j]) << ' ';
+            }
         }
        std::cout << "]" << std::endl;
     }
--- src/state.h
+++ src/state.h
@@ -5,10 +5,10 @@
 class State {
 public:
     State();
-    void addFence(bool horizontal, uint8_t row, uint8_t col);
+    void addFence(bool horizontal, std::pair<uint8_t, uint8_t> fence);
     const std::vector<std::vector<uint8_t>>& getGraph();
-    void setP1(uint8_t row, uint8_t col);
-    void setP2(uint8_t row, uint8_t col);
+    void setP1(std::pair<uint8_t, uint8_t> field);
+    void setP2(std::pair<uint8_t, uint8_t> field);
     std::pair<uint8_t, uint8_t> getP1() const;
     std::pair<uint8_t, uint8_t> getP2() const;
     void dirtyPrint() const;
@@ -18,8 +18,12 @@ public:
 private:
     uint8_t board_size;
     std::vector<std::vector<uint8_t>> graph;
-    std::pair<uint8_t, uint8_t> p1;
-    std::pair<uint8_t, uint8_t> p2;
+    std::vector<std::pair<uint8_t, uint8_t>> horizontalFences;
+    std::vector<std::pair<uint8_t, uint8_t>> verticalFences;
+    std::pair<uint8_t, uint8_t> p1; // [0, 0] - [8, 8]
+    std::pair<uint8_t, uint8_t> p2; // [0, 0] - [8, 8]
     uint8_t p1Fences;
     uint8_t p2Fences;
+    void deleteVerticalEdge(uint8_t top, uint8_t bottom);
+    void deleteHorizontalEdge(uint8_t left, uint8_t right);
 };
