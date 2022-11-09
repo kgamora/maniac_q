7d963291ec2decc09a7ecde22945dc90d5be580f
reallybigsky <do.not.focus@gmail.com>
2022-11-09 13:28:24 +0300

    refactor engine

--- include/engine.hpp
+++ include/engine.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "board.hpp"
+
 #include <cstdint>
 #include <vector>
 
-uint8_t distanceToBase(const uint8_t position, const uint8_t active_player);
-uint8_t positionDifference(const uint8_t min_player_position, const uint8_t max_player_position, const uint8_t active_player);
-uint8_t movesToNextColumn(const std::vector<std::vector<uint8_t>>& board, const uint8_t player_position, const uint8_t active_player);
+uint8_t distanceToBase(uint8_t position, uint8_t active_player);
+uint8_t positionDifference(uint8_t min_player_position, uint8_t max_player_position, uint8_t active_player);
+uint8_t movesToNextColumn(const Board& board, uint8_t player_position, uint8_t active_player);
--- src/engine.cpp
+++ src/engine.cpp
@@ -1,50 +1,51 @@
+#include "engine.hpp"
+
 #include <vector>
 #include <queue>
 #include <cstdint>
 
 // EXTEND ALL THIS FUNCTIONS FOR MORE THAN 2 PLAYERS
 
-uint8_t distanceToBase(const uint8_t position, const uint8_t active_player) {
+uint8_t distanceToBase(uint8_t position, uint8_t active_player) {
     if (active_player == 1) {
-        return position / 9;
+        return position / BOARD_SIDE_LENGTH;
     } else {
-        return (80 - position) / 9;
+        return ((BOARD_SIZE - 1) - position) / BOARD_SIDE_LENGTH;
     }
 }
 
 // I have douts about validity of this function
-uint8_t positionDifference(const uint8_t min_player_position, const uint8_t max_player_position, const uint8_t active_player) {
+uint8_t positionDifference(uint8_t min_player_position, uint8_t max_player_position, uint8_t active_player) {
     uint8_t position_max = distanceToBase(max_player_position, active_player);
     uint8_t position_min = distanceToBase(min_player_position, active_player % 2 + 1);
 
     return position_max - position_min;
 }
 
-uint8_t movesToNextColumn(const std::vector<std::vector<uint8_t>>& board, const uint8_t player_position, const uint8_t active_player) {
+uint8_t movesToNextColumn(const Board& board, uint8_t player_position, uint8_t active_player) {
     uint8_t depth = 0;
     std::queue<uint8_t> q;
 
     q.push(player_position);
     size_t nodes_on_level = q.size();
 
-    std::vector<bool> visited(81, false);
+    std::vector<uint8_t> visited(BOARD_SIZE, false);
 
     while (!q.empty()) {
-
         while (nodes_on_level != 0) {
             uint8_t current_position = q.front();
 
             if (active_player == 1) {
-                if (current_position / 9 == player_position / 9 + 1) {
+                if (current_position / BOARD_SIDE_LENGTH == player_position / BOARD_SIDE_LENGTH + 1) {
                     return depth;
                 }
             } else {
-                if (current_position / 9 + 1 == player_position / 9) {
+                if (current_position / BOARD_SIDE_LENGTH + 1 == player_position / BOARD_SIDE_LENGTH) {
                     return depth;
                 }
             }
 
-            for (int8_t position : board[current_position]) {
+            for (uint8_t position : board.getGraph()[current_position]) {
                 if (!visited[position]) {
                     q.push(position);
                 }
